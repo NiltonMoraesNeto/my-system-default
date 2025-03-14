4a2014fe7df475f197fbd7b130ca55adbdb8621a
Nilton Moraes Neto <nilton.nmn@gmail.com>
2025-03-14 10:33:39 -0300

    add translation and eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,45 @@
 import React from "react";
 import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ThemeProvider } from "./components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-import { I18nProvider } from "./components/I18nProvider";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import {ThemeProvider} from "./components/theme-provider";
+import {Toaster} from "@/components/ui/sonner";
+import {I18nProvider} from "./components/I18nProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Meu Sistema",
-  description: "Sistema de gerenciamento",
+    title: "Meu Sistema",
+    description: "Sistema de gerenciamento",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR" suppressHydrationWarning>
-      <head>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="script-src 'self' 'unsafe-eval' 'unsafe-inline'"
-        />
-      </head>
-      <body className={inter.className}>
+    return (
+        <html lang="pt-BR" suppressHydrationWarning>
+        <head>
+            <meta
+                httpEquiv="Content-Security-Policy"
+                content="script-src 'self' 'unsafe-eval' 'unsafe-inline'"
+            />
+            <title>System Default</title>
+        </head>
+        <body className={inter.className}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
         >
-          <I18nProvider>
-            {children}
-            <Toaster />
-          </I18nProvider>
+            <I18nProvider>
+                {children}
+                <Toaster/>
+            </I18nProvider>
         </ThemeProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,96 +1,96 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { createLogin, login } from "@/services/login";
-import { toast } from "sonner";
-import { usuarioPost } from "@/services/usuario";
-import { useTranslation } from "react-i18next";
-import { FormCadPassword } from "@/components/common/form-cad-password";
+import React, {useState} from "react";
+import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {createLogin, login} from "@/services/login";
+import {toast} from "sonner";
+import {usuarioPost} from "@/services/usuario";
+import {useTranslation} from "react-i18next";
+import {FormCadPassword} from "@/components/common/form-cad-password";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newEmail, setNewEmail] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [error, setError] = useState("");
-  const [newError, setNewError] = useState("");
-  const [isOpen, setIsOpen] = useState(false);
-  const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newEmail, setNewEmail] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [error, setError] = useState("");
+    const [newError, setNewError] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+    const router = useRouter();
 
-  const { t } = useTranslation();
+    const {t} = useTranslation();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError("");
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError("");
 
-    const success = await login(email, password);
-    if (success) {
-      router.push("/dashboard");
-    } else {
-      setError("Credenciais inválidas");
-    }
-  };
+        const success = await login(email, password);
+        if (success) {
+            router.push("/dashboard");
+        } else {
+            setError("Credenciais inválidas");
+        }
+    };
 
-  const handleSubmitNewAccount = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setNewError("");
-    const success = await createLogin(newEmail, newPassword);
+    const handleSubmitNewAccount = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setNewError("");
+        const success = await createLogin(newEmail, newPassword);
 
-    if (success) {
-      const newAccount = { email: newEmail };
-      await usuarioPost(newAccount);
-      setIsOpen(false);
-      toast.success(t("createLogin.apiTitle"), {
-        description: t("createLogin.apiMsgSuccess"),
-      });
-    } else {
-      toast.error(t("createLogin.apiTitle"), {
-        description: t("createLogin.apiMsgError"),
-      });
-      setNewError(t("createLogin.apiMsgError"));
-    }
-  };
+        if (success) {
+            const newAccount = {email: newEmail};
+            await usuarioPost(newAccount);
+            setIsOpen(false);
+            toast.success(t("createLogin.apiTitle"), {
+                description: t("createLogin.apiMsgSuccess"),
+            });
+        } else {
+            toast.error(t("createLogin.apiTitle"), {
+                description: t("createLogin.apiMsgError"),
+            });
+            setNewError(t("createLogin.apiMsgError"));
+        }
+    };
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 bg-background">
-      <div className="w-full max-w-sm md:max-w-md p-4 md:p-8 bg-card rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold text-center mb-2">
-          {t("login.title")}
-        </h1>
-        {error && <p className="text-red-500 text-center">{error}</p>}
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <Input
-            type="email"
-            placeholder={t("login.placeholderEmail")}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <Input
-            type="password"
-            placeholder={t("login.placeholderPassword")}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <Button type="submit" className="w-full">
-            {t("login.buttonEnter")}
-          </Button>
-        </form>
-      </div>
-      <FormCadPassword
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        newError={newError}
-        handleSubmitNewAccount={handleSubmitNewAccount}
-        newEmail={newEmail}
-        setNewEmail={setNewEmail}
-        newPassword={newPassword}
-        setNewPassword={setNewPassword}
-      />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen px-4 bg-background">
+            <div className="w-full max-w-sm md:max-w-md p-4 md:p-8 bg-card rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold text-center mb-2">
+                    {t("login.title")}
+                </h1>
+                {error && <p className="text-red-500 text-center">{error}</p>}
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <Input
+                        type="email"
+                        placeholder={t("login.placeholderEmail")}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <Input
+                        type="password"
+                        placeholder={t("login.placeholderPassword")}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <Button type="submit" className="w-full">
+                        {t("login.buttonEnter")}
+                    </Button>
+                </form>
+            </div>
+            <FormCadPassword
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                newError={newError}
+                handleSubmitNewAccount={handleSubmitNewAccount}
+                newEmail={newEmail}
+                setNewEmail={setNewEmail}
+                newPassword={newPassword}
+                setNewPassword={setNewPassword}
+            />
+        </div>
+    );
 }
